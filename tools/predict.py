658c81031d3f6a14f0f20d44cf7ec184eb733717
andrey1908 <andrey.krishtopik@mail.ru>
2020-04-13 09:59:28 +0300

    add predict.py and report.py

---
+++ tools/predict.py
@@ -0,0 +1,130 @@
+import os
+from mmdet.apis import init_detector, inference_detector
+import mmcv
+from PIL import Image
+from tqdm import tqdm
+import argparse
+import json
+
+
+def build_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-cfg', '--config-file', required=True, type=str)
+    parser.add_argument('-ch', '--checkpoint-file', required=True, type=str)
+    parser.add_argument('-out', '--out-file', required=True, type=str)
+    parser.add_argument('-set', '--set-of-data', type=str, choices=['train', 'val', 'test'], default='val')
+    parser.add_argument('-dets-only', '--detections-only', action='store_true')
+    parser.add_argument('-img-fld', '--images-folder', type=str)
+    parser.add_argument('-img', '--images-file', type=str)
+    parser.add_argument('-thr', '--threshold', type=float, default=0.)
+    parser.add_argument('-mmdet-fld', '--mmdetection-folder', type=str, default='')
+    parser.add_argument('-gpu', '--gpu', type=int, default=0)
+    return parser
+
+
+def get_images(images_folder, images_file=None):
+    if images_file is None:
+        return get_images_from_folder(images_folder)
+    if images_file.endswith('.json'):
+        return get_images_from_json(images_folder, images_file)
+    return None, None, None
+
+
+def get_images_from_folder(images_folder):
+    images_names = sorted(os.listdir(images_folder))
+    images_ids, images_files = list(), list()
+    image_id = 0
+    for image_name in images_names:
+        images_ids.append(image_id)
+        image_id += 1
+        images_files.append(os.path.join(images_folder, image_name))
+    return images_names, images_ids, images_files
+
+
+def get_images_from_json(images_folder, json_file):
+    with open(json_file, 'r') as f:
+        json_dict = json.load(f)
+    images = json_dict['images']
+    images_names, images_ids, images_files = list(), list(), list()
+    for image in images:
+        images_names.append(image['file_name'])
+        images_ids.append(image['id'])
+        images_files.append(os.path.join(images_folder, image['file_name']))
+    return images_names, images_ids, images_files
+
+
+def init_coco(classes):
+    json_dict = {'images': list(), 'annotations': list(), 'categories': list()}
+    for class_id, class_name in enumerate(classes):
+        category = {'name': class_name, 'id': class_id+1}
+        json_dict['categories'].append(category)
+    return json_dict
+
+
+def add_predictions_to_coco(image_file, image_id, image_name, predictions, threshold, json_dict):
+    image = dict()
+    image['id'] = image_id
+    image['file_name'] = image_name
+    w, h = Image.open(image_file).size
+    image['width'] = w
+    image['height'] = h
+    json_dict['images'].append(image)
+
+    num_classes = len(json_dict['categories'])
+    for cl_id in range(num_classes):
+        for k in range(len(predictions[cl_id])):
+            if predictions[cl_id][k][4] < threshold:
+                continue
+            annotation = dict()
+            if len(json_dict['annotations']) == 0:
+                annotation['id'] = 1
+            else:
+                annotation['id'] = json_dict['annotations'][-1]['id'] + 1
+            annotation['iscrowd'] = 0
+            annotation['image_id'] = image_id
+            annotation['category_id'] = cl_id + 1
+            xtl, ytl, xbr, ybr, score = map(float, predictions[cl_id][k])
+            annotation['bbox'] = [max(xtl, 0), max(ytl, 0),
+                                  min(xbr, w) - max(xtl, 0),
+                                  min(ybr, h) - max(ytl, 0)]
+            annotation['area'] = annotation['bbox'][2] * annotation['bbox'][3]
+            annotation['score'] = score
+            json_dict['annotations'].append(annotation)
+
+
+def complete_args(config_file, set_of_data, images_folder, images_file, mmdetection_folder):
+    cfg = mmcv.Config.fromfile(config_file)
+    if images_folder is None:
+        images_folder = os.path.join(mmdetection_folder, cfg.data[set_of_data].img_prefix)
+        # if images_folder is not None, images_file can be None
+        if images_file is None:
+            images_file = os.path.join(mmdetection_folder, cfg.data[set_of_data].ann_file)
+    return images_folder, images_file
+
+
+def predict(config_file, checkpoint_file, out_file=None, detections_only=False, set_of_data='val',
+            images_folder=None, images_file=None, threshold=0., mmdetection_folder=''):
+    assert set_of_data in ['train', 'val', 'test']
+    images_folder, images_file = complete_args(config_file, set_of_data, images_folder, images_file, mmdetection_folder)
+    model = init_detector(config_file, checkpoint_file, device='cuda:0')
+    classes = model.CLASSES
+    images_names, images_ids, images_files = get_images(images_folder, images_file)
+    json_dict = init_coco(classes)
+    for image_name, image_id, image_file in tqdm(list(zip(images_names, images_ids, images_files))):
+        predictions = inference_detector(model, image_file)
+        add_predictions_to_coco(image_file, image_id, image_name, predictions, threshold, json_dict)
+    if detections_only:
+        json_dict = json_dict['annotations']
+    if out_file:
+        with open(out_file, 'w') as f:
+            json.dump(json_dict, f, indent=2)
+    return json_dict
+
+
+if __name__ == '__main__':
+    parser = build_parser()
+    args = parser.parse_args()
+    os.environ["CUDA_VISIBLE_DEVICES"] = str(args.gpu)
+    kwargs = vars(args)
+    kwargs.pop('gpu')
+    predict(**kwargs)
---
+++ tools/report.py
@@ -0,0 +1,188 @@
+import os
+import matplotlib.pyplot as plt
+from tools.predict import predict
+from dataset_scripts.metrics_eval import evaluate_detections, extract_mAP, extract_AP, get_classes
+from dataset_scripts.coco_boxes import leave_boxes, leave_boxes_scale
+from tqdm import tqdm
+import argparse
+import mmcv
+import csv
+import json
+import threading
+
+
+def build_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-cfg', '--config-file', required=True, type=str)
+    parser.add_argument('-set', '--set-of-data', type=str, choices=['train', 'val', 'test'], default='val')
+    parser.add_argument('-ch-fld', '--checkpoints-folder', type=str)
+    parser.add_argument('-rep-fld', '--report-folder', type=str)
+    parser.add_argument('-img-fld', '--images-folder', type=str)
+    parser.add_argument('-ann', '--annotations-file', type=str)
+    parser.add_argument('-area', '--area', nargs=2, type=str, default=['0**2', '1e5**2'])
+    parser.add_argument('-shape', '--shape', nargs=2, type=int)
+    parser.add_argument('-add', '--add', action='store_true')
+    parser.add_argument('-dont-repredict', '--dont-repredict', dest='repredict', action='store_false')
+    parser.add_argument('-mmdet-fld', '--mmdetection-folder', type=str, default='')
+    parser.add_argument('-gpu', '--gpu', type=int, default=0)
+    return parser
+
+
+def get_existing_information(report_folder):
+    existing_epochs, existing_metrics = list(), list()
+    with open(os.path.join(report_folder, 'metrics.csv'), 'r') as f:
+        existing_information = csv.reader(f, delimiter=' ')
+        for new_information in existing_information:
+            existing_epochs.append(int(new_information[0]))
+            existing_metrics.append(list(map(float, new_information[1:])))
+    return existing_epochs, existing_metrics
+
+
+def create_folders(report_folder):
+    if not os.path.exists(report_folder):
+        os.mkdir(report_folder)
+    if not os.path.exists(os.path.join(report_folder, 'predictions')):
+        os.mkdir(os.path.join(report_folder, 'predictions'))
+
+
+def get_checkpoint_files(checkpoints_folder, existing_epochs):
+    checkpoint_files = os.listdir(checkpoints_folder)
+    epochs = []
+    for checkpoint_file in checkpoint_files:
+        if checkpoint_file.startswith("epoch"):
+            if not int(checkpoint_file[6:-4]) in existing_epochs:
+                epochs.append(int(checkpoint_file[6:-4]))
+    checkpoint_files = []
+    for epoch in epochs:
+        checkpoint_files.append(os.path.join(checkpoints_folder, "epoch_{}.pth".format(epoch)))
+    return checkpoint_files, epochs
+
+
+def run_models(config_file, checkpoint_files, epochs, report_folder, images_folder, annotations_file, repredict=True):
+    for checkpoint_file, epoch in tqdm(list(zip(checkpoint_files, epochs))):
+        out_file = os.path.join(report_folder, 'predictions', 'epoch_{}.json'.format(epoch))
+        if os.path.exists(out_file) and not repredict:
+            continue
+        predict(config_file, checkpoint_file, out_file, detections_only=True, images_folder=images_folder, images_file=annotations_file)
+
+
+def write_json_dict(json_dict, w):
+    with open(w, 'w') as f:
+        json.dump(json_dict, f)
+
+
+def calculate_metrics(epochs, report_folder, annotations_file, area, shape=None):
+    metrics = list()
+    # kostil' #
+    indexes_to_correct = list()
+    ###########
+    with open(annotations_file, 'r') as f:
+        annotations_dict = json.load(f)
+    if shape is None:
+        leave_boxes(annotations_dict, area)
+    else:
+        leave_boxes_scale(annotations_dict, area, shape[0], shape[1])
+
+    for epoch in tqdm(epochs):
+        detections_file = os.path.join(report_folder, 'predictions/epoch_{}.json'.format(epoch))
+        with open(detections_file, 'r') as f:
+            detections_dict = json.load(f)
+        # kostil' #
+        if detections_dict == list():
+            metrics.append(None)
+            indexes_to_correct.append(len(metrics)-1)
+            continue
+        ###########
+        detections_dict_with_images = {'images': annotations_dict['images'], 'annotations': detections_dict}
+        if shape is None:
+            leave_boxes(detections_dict_with_images, area)
+        else:
+            leave_boxes_scale(detections_dict_with_images, area, shape[0], shape[1])
+        detections_dict = detections_dict_with_images['annotations']
+
+        annotations_dict_r, annotations_dict_w = os.pipe()
+        annotations_dict_writing = threading.Thread(target=write_json_dict, args=(annotations_dict, annotations_dict_w))
+        annotations_dict_writing.start()
+        detections_dict_r, detections_dict_w = os.pipe()
+        detections_dict_writing = threading.Thread(target=write_json_dict, args=(detections_dict, detections_dict_w))
+        detections_dict_writing.start()
+        results = evaluate_detections(annotations_dict_r, detections_dict_r)
+        classes = get_classes(results)
+        metric = [extract_mAP(results)]
+        metric += extract_AP(results, classes)
+        metrics.append(metric)
+    # kostil' #
+    for index in indexes_to_correct:
+        metrics[index] = [0] * (len(classes)+1)
+    ###########
+    return metrics, classes
+
+
+def save_metrics(epochs, metrics, classes, report_folder):
+    with open(os.path.join(report_folder, 'metrics.csv'), 'w') as f:
+        writer = csv.writer(f, delimiter=' ')
+        for epoch, metric in zip(epochs, metrics):
+            writer.writerow([epoch] + metric)
+    with open(os.path.join(report_folder, 'classes.txt'), 'w') as f:
+        for i, cl in enumerate(classes):
+            f.write(cl)
+            if i != len(classes)-1:
+                f.write(' ')
+    mAP = list()
+    APs = list()
+    for metric in metrics:
+        mAP.append(metric[0])
+        APs.append(metric[1:])
+    plt.plot(epochs, mAP)
+    plt.grid()
+    plt.savefig(os.path.join(report_folder, 'mAP.png'))
+    plt.close()
+    plt.plot(epochs, APs)
+    plt.grid()
+    plt.savefig(os.path.join(report_folder, 'APs.png'))
+    plt.close()
+
+
+def complete_args(config_file, set_of_data, checkpoints_folder, report_folder, images_folder, annotations_file,
+                  mmdetection_folder):
+    cfg = mmcv.Config.fromfile(config_file)
+    if checkpoints_folder is None:
+        checkpoints_folder = os.path.join(mmdetection_folder, cfg.work_dir)
+    if report_folder is None:
+        report_folder = os.path.join(mmdetection_folder, cfg.work_dir, 'report')
+    if images_folder is None:
+        images_folder = os.path.join(mmdetection_folder, cfg.data[set_of_data].img_prefix)
+    if annotations_file is None:
+        annotations_file = os.path.join(mmdetection_folder, cfg.data[set_of_data].ann_file)
+    return checkpoints_folder, report_folder, images_folder, annotations_file
+
+
+def report(config_file, checkpoints_folder=None, report_folder=None, images_folder=None, annotations_file=None,
+           set_of_data='val', area=(0**2, 1e5**2), shape=None, add=False, repredict=True, mmdetection_folder=''):
+    assert set_of_data in ['train', 'val', 'test']
+    checkpoints_folder, report_folder, images_folder, annotations_file = complete_args(config_file, set_of_data,
+                                checkpoints_folder, report_folder, images_folder, annotations_file, mmdetection_folder)
+    if area[1] == -1:
+        area = (area[0], 1e5 ** 2)
+    if add:
+        existing_epochs, existing_metrics = get_existing_information(report_folder)
+    else:
+        existing_epochs, existing_metrics = list(), list()
+    create_folders(report_folder)
+    checkpoint_files, epochs = get_checkpoint_files(checkpoints_folder, existing_epochs)
+    run_models(config_file, checkpoint_files, epochs, report_folder, images_folder, annotations_file, repredict)
+    metrics, classes = calculate_metrics(epochs, report_folder, annotations_file, area, shape)
+    epochs += existing_epochs
+    metrics += existing_metrics
+    epochs, metrics = zip(*sorted(zip(epochs, metrics)))
+    save_metrics(epochs, metrics, classes, report_folder)
+
+
+if __name__ == '__main__':
+    parser = build_parser()
+    args = parser.parse_args()
+    os.environ["CUDA_VISIBLE_DEVICES"] = str(args.gpu)
+    args.area = list(map(eval, args.area))
+    kwargs = vars(args)
+    kwargs.pop('gpu')
+    report(**kwargs)
