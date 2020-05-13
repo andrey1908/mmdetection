221113ab86f9b1a467adfd331b57cd00c52e3f47
andrey1908 <andrey.krishtopik@mail.ru>
2020-05-13 14:53:18 +0300

    add time in log and some slight changes

--- mmdet/apis/hooks.py
+++ mmdet/apis/hooks.py
@@ -1,4 +1,5 @@
 from mmcv.runner.hooks import HOOKS, Hook, LrUpdaterHook
+import time
 
 
 class CustomLog(Hook):
@@ -8,12 +9,16 @@ class CustomLog(Hook):
         self.out_file = out_file
         self.when_defrost = when_defrost
         self.out = None
+        self.t = None
 
     def before_run(self, runner):
         if self.out_file:
             self.out = open(self.out_file, 'a')
+
+    def before_train_epoch(self, runner):
+        self.t = time.time()
         if self.when_defrost is not None:
-            if runner.epoch == self.when_defrost == 0:
+            if runner.epoch == self.when_defrost:
                 log_line = 'Defrosting backbone'
                 print(log_line)
                 if self.out:
@@ -24,23 +29,19 @@ class CustomLog(Hook):
             self.avg_loss = runner.outputs['loss']
         else:
             self.avg_loss = self.avg_loss * 0.9 + runner.outputs['loss'] * 0.1
-        log_line = '{}: {:.5}, {:.5} avg, {:.3} rate, {} images'.format(runner.iter+1, runner.outputs['loss'], self.avg_loss,
-                                                                        runner.current_lr()[0], (runner.iter+1)*self.batch_size)
+        log_line = '{}: {:.5}, {:.5} avg, {:.3} rate, {:.3} seconds, {} images'.format(runner.iter+1,
+            runner.outputs['loss'], self.avg_loss, runner.current_lr()[0], time.time()-self.t,
+            (runner.iter+1)*self.batch_size)
         print(log_line)
         if self.out:
             self.out.write(log_line + '\n')
+        self.t = time.time()
 
     def after_train_epoch(self, runner):
         log_line = 'Saving weights (epoch {})'.format(runner.epoch+1)
         print(log_line)
         if self.out:
             self.out.write(log_line + '\n')
-        if self.when_defrost is not None:
-            if (runner.epoch+1) == self.when_defrost:
-                log_line = 'Defrosting backbone'
-                print(log_line)
-                if self.out:
-                    self.out.write(log_line + '\n')
 
     def after_run(self, runner):
         if self.out:
